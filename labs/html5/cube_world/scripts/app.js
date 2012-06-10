a74858ea4e122693c4eca7f653ba2dafa8b2549c
Tong <lythm780522@gmail.com>
2012-06-11 01:12:43 +0800

    aa

--- labs/html5/cube_world/scripts/app.js
+++ labs/html5/cube_world/scripts/app.js
@@ -11,7 +11,6 @@ function Application()
 
 		this.date = new Date();
 
-		this.fps = fps;
 		this.screen = document.getElementById(screen_id);
 		
 		this.screen_ctx = this.screen.getContext("2d");
@@ -25,32 +24,50 @@ function Application()
 				
 		this.entity_manager = new EntityManager();
 
-
 		window.setInterval(this.update.bind(this), 1000 / fps);
 
-		//document.getElementById("output").value = fps;
 
-		var t = [];
-
-		t[0] = "a";
-		t[1] = "b";
-
-		alert(t[0] + "," + t[1]);
-		
+		this.frame_count = 0;
+		this.frame_tick = 0;
 	}
 	
+	this.draw_fps = function(tick)
+	{
+		var dt = tick - this.frame_tick;
+		if(dt > 1000)
+		{
+			this.fps = this.frame_count * 1000 / dt;
+			this.frame_tick = tick;
+			this.frame_count = 0;
+		}
+		else
+		{
+			this.frame_count ++;
+		}
+		
+		this.screen_ctx.fillStyle = "RGB(255,255,255)";
+
+		this.screen_ctx.fillText("fps: " + this.fps.toFixed(3), 10, 10);
+
+	}
 	this.update = function()
 	{
-		
-		var tick = this.date.getTime();
-
-		this.entity_manager.update(tick);
+		var date = new Date();
+		var tick = date.getTime();
 
 		this.screen_ctx.clearRect(0, 0, this.screen.width, this.screen.height); 
+
+		this.screen_ctx.fillStyle = "RGB(0,0,0)";
+
+		this.screen_ctx.fillRect(0, 0, this.screen.width, this.screen.height); 
+
+		this.entity_manager.update(tick);
+		
 		this.render_manager.render();
 
-		//document.getElementById("output").value += this.world.width + "," + this.world.height + "\n";
-		//document.getElementById("output").value += this.fps + "<br>";
+
+		this.draw_fps(tick);
+		
 	}
 
 }
