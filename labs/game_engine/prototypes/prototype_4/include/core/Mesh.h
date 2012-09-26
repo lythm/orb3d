c044c5c1ba502c14a9bc9a6e1aa982d2d24564aa
Tong <lythm780522@gmail.com>
2012-09-26 17:43:42 +0800

    g7

--- labs/game_engine/prototypes/prototype_4/include/core/Mesh.h
+++ labs/game_engine/prototypes/prototype_4/include/core/Mesh.h
@@ -6,8 +6,35 @@ namespace engine
 	class Mesh
 	{
 	public:
+
+		class SubMesh
+		{
+		public:
+			
+			
+
+		private:
+
+			void*									m_pIndexBuffer;
+			void*									m_pVertexBuffer;
+
+			int										m_vertexCount;
+			int										m_indexCount;
+			int										m_primitiveCount;
+
+			GFXPtr									m_pGFX;
+
+			RenderDataPtr							m_pRenderData;
+		};
+
 		Mesh(void);
 		virtual ~Mesh(void);
+
+
+	private:
+		void*										m_pIndexBuffer;
+		void*										m_pVertexBuffer;
+
 	};
 
 
