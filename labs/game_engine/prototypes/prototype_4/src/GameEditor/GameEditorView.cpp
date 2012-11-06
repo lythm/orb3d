70326d81f6cc3c8bf0046ed961518b2b2f72d237
Tong <lythm780522@gmail.com>
2012-11-06 18:00:45 +0800

    g36

---
+++ labs/game_engine/prototypes/prototype_4/src/GameEditor/GameEditorView.cpp
@@ -0,0 +1,96 @@
+
+// GameEditorView.cpp : CGameEditorView 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "GameEditor.h"
+#endif
+
+#include "GameEditorDoc.h"
+#include "GameEditorView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CGameEditorView
+
+IMPLEMENT_DYNCREATE(CGameEditorView, CView)
+
+BEGIN_MESSAGE_MAP(CGameEditorView, CView)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CGameEditorView 构造/析构
+
+CGameEditorView::CGameEditorView()
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CGameEditorView::~CGameEditorView()
+{
+}
+
+BOOL CGameEditorView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CGameEditorView 绘制
+
+void CGameEditorView::OnDraw(CDC* /*pDC*/)
+{
+	CGameEditorDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+void CGameEditorView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CGameEditorView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CGameEditorView 诊断
+
+#ifdef _DEBUG
+void CGameEditorView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CGameEditorView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CGameEditorDoc* CGameEditorView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CGameEditorDoc)));
+	return (CGameEditorDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CGameEditorView 消息处理程序
---
+++ labs/game_engine/prototypes/prototype_4/src/GameEditor/GameEditorView.h
@@ -0,0 +1,49 @@
+
+// GameEditorView.h : CGameEditorView 类的接口
+//
+
+#pragma once
+
+
+class CGameEditorView : public CView
+{
+protected: // 仅从序列化创建
+	CGameEditorView();
+	DECLARE_DYNCREATE(CGameEditorView)
+
+// 特性
+public:
+	CGameEditorDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~CGameEditorView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // GameEditorView.cpp 中的调试版本
+inline CGameEditorDoc* CGameEditorView::GetDocument() const
+   { return reinterpret_cast<CGameEditorDoc*>(m_pDocument); }
+#endif
+
